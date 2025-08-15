eeef84d51f03321fb5468c30be129ce1c47772b5
olabams <bamgboseolawale20@gmail.com>
2025-08-15 15:55:46 +0100

    Friday

--- Task7/Task_1_of_5.py
+++
@@ -1,4 +0,0 @@
-# Task1:  Create and Display
-dishes = tuple(input("Enter dish:") for _ in range(3))
-print(", ".join(dishes))
-print("\n".join(dishes))
--- Task7/Task_2_of_5.py
+++
@@ -1,3 +0,0 @@
-# Task2: Tuple and Input
-friends = tuple(input("Enter name:") for _ in range (5))
-print(friends[::-1])
